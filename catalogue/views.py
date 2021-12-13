3df5c2ce5081fc3b20ee073f0770ea1829682438
NianhengWu <nianheng.wu@ims.uni-stuttgart.de>
2021-12-14 00:42:04 +0100

    graph path

--- catalogue/views.py
+++ catalogue/views.py
@@ -99,7 +99,8 @@ def _stylize_graphs(tree, file_path, target_graph_dir, content):
     prefix = os.path.relpath(target_graph_dir, settings.AWS_URL)+'/'
     images = bucket.objects.filter(Delimiter='/', Prefix=prefix)
     if images:
-        content["graphs"] = [os.path.join(prefix, img.key) for img in images]
+        static_rel_path = os.path.relpath(target_graph_dir, settings.STATIC_URL) + '/'
+        content["graphs"] = [os.path.join(static_rel_path, img.key) for img in images]
     else:
         raise (FileNotFoundError('weird'))
         make_graphs(tree, file_path, settings.GRAPH_DIR)

d3c2a36f9aae4eac3cb0f82aa19586b9bb5ed886
NianhengWu <nianheng.wu@ims.uni-stuttgart.de>
2021-12-14 01:07:05 +0100

    graph path

--- catalogue/views.py
+++ catalogue/views.py
@@ -101,7 +101,7 @@ def _stylize_graphs(tree, file_path, target_graph_dir, content):
     if images:
         static_rel_path = os.path.relpath(target_graph_dir, settings.STATIC_URL) + '/'
         content["graphs"] = [os.path.join(static_rel_path, img.key) for img in images]
-        raise (FileNotFoundError(target_graph_dir, str(content["graphs"][0])))
+        raise (FileNotFoundError(settings.GRAPH_DIR, static_rel_path, target_graph_dir, str(content["graphs"][0])))
     else:
         make_graphs(tree, file_path, settings.GRAPH_DIR)
         try:

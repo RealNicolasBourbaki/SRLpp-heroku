56bb0b1b06d1b2c1008213464c87426493cc8283
NianhengWu <nianheng.wu@ims.uni-stuttgart.de>
2022-01-25 11:38:33 +0100

    signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from .views import MyPasswordResetView, reset_password, signup, activate
 
 
 urlpatterns = [
-    path('signup/', signup(), name='signup'),
+    path('signup/', signup, name='signup'),
     path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate, name='activate'),
-    path('password_reset/', MyPasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/', MyPasswordResetView.as_view, name="password_reset"),
 ]

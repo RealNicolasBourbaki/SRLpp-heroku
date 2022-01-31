8f81570719b6937570ef5f346d1a5b55c96e8bb0
NianhengWu <nianheng.wu@ims.uni-stuttgart.de>
2022-01-31 15:34:11 +0100

    reset pw

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from .views import MyPasswordResetView, reset_password, signup, activate
+from django.contrib.auth import views as auth_views
+from .views import signup, activate
 
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate, name='activate'),
-    path('password_reset/', MyPasswordResetView.as_view, name="password_reset"),
+    path("password-reset", auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"),
+         name="password_reset"),
+    path("password-reset/done/",
+         auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
+         name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>",
+         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"),
+         name="password_reset_confirm"),
+    path("password-reset-complete/",
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
+         name="password_reset_complete")
+    ,
 ]

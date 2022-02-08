d9d675ab1efac01b3be98aa2791b15d1f7e20203
NianhengWu <nianheng.wu@ims.uni-stuttgart.de>
2022-02-08 14:35:30 +0100

    generating sgs

--- catalogue/views.py
+++ catalogue/views.py
@@ -342,7 +342,7 @@ def generate_sg(file_path, name):
     out_path = settings.TEMP_DIR
     make_graphs(tree, file_path, out_path)
     try:
-        graphs = os.listdir(out_path.joinpath(name[:-4]))
+        graphs = os.listdir(os.path.join(out_path, name[:-4]))
 
     except FileNotFoundError:
         graphs = []
